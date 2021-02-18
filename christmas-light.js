d12bc88f5409f2f7cf13e1ac0de48016d419aab6
Gabriel Stefano <gabriel.lrnc@gmail.com>
2021-02-18 12:51:34 -0300

    not finished

--- christmas-light.js
+++ christmas-light.js
@@ -13,11 +13,11 @@ function TurnOn(){
             // var stylesheet = createStyleSheet();
             // stylesheet.insertRule(getSelector() + `@keyframes glow-${j} { 0%, ${t1}% { box-shadow: 0 0 20px 5px #c0392b; } ${t2}% { box-shadow: none; } }`, 0);
             // CSSKeyframesRule.appendRule(`@keyframes glow-${j} { 0%, ${t1}% { box-shadow: 0 0 20px 5px #c0392b; } ${t2}% { box-shadow: none; } }`)
-            insertStyleSheetRule(`@keyframes glow-${j} { 0%, ${t1}% { box-shadow: 0 0 20px 5px #c0392b; } ${t2}% { box-shadow: none; } }`);
+            // insertStyleSheetRule(`@keyframes glow-${j} { 0%, ${t1}% { box-shadow: 0 0 20px 5px #c0392b; } ${t2}% { box-shadow: none; } }`);
             circle[i].removeAttribute("style");
             circle[i].style.animationPlayState = "running"
 
-            circle[i].style.animation = `glow-${j} 1s infinite`
+            circle[i].style.animation = `glow-${n} 1s infinite`
             // circle[i].style.setProperty('--m','50%');
             // circle[i].style.setProperty('--w','100%');
             n++
@@ -53,9 +53,10 @@ var item = document.querySelectorAll('.circle').forEach(item =>{
         var colors = ['red', 'yellow', 'blue', 'green']
         for (var i=0; i<colors.length; i++){            
             if (i<(colors.length-1) && item.classList==`circle ${colors[i]}`){
+                // window.alert(item.style.animation)
                 item.classList.remove(`${colors[i]}`)
                 item.classList.toggle(`${colors[(i+1)]}`)
-                // window.alert(item.classList[1])
+                // window.alert(item.style.animation)
                 // item.style.animation = `inherit`
                 // item.style.animation = 'paused'
                 break;
@@ -63,27 +64,44 @@ var item = document.querySelectorAll('.circle').forEach(item =>{
             else if(i==(colors.length-1)){
                 item.classList.remove(`${colors[i]}`)
                 item.classList.toggle(`${colors[0]}`)
-
+                break;
                 // item.style.animation = `glow-${1} 1s infinite`
                 // item.style.animation = 'inherit'
             }
+            // var cont = a
+
+        }
+        if (item.style.animation.indexOf('none') == item.style.animation.lastIndexOf('none')){
+            // window.alert(item.style.animation)
+            // 0s ease 0s 1 normal none running none
+            // 1s ease 0s infinite normal none running glow-1
+
+            if (item.classList[1] == 'red'){
+                item.style.animation = '1s ease 0s infinite normal none running glow-1'
+            }
+            if (item.classList[1] == 'yellow'){
+                item.removeAttribute("style");
+                item.style.animationPlayState = "running"
+                item.style.animation = `glow-2 1s infinite`
+            }
+            // if (item.classList[1])
         }
 
     })
 })
 
 
-function insertStyleSheetRule(ruleText){
-    let sheets = document.styleSheets;
+// function insertStyleSheetRule(ruleText){
+    // let sheets = document.styleSheets;
 
-    if(sheets.length == 0)
-    {
-        let style = document.createElement('style');
-        style.appendChild(document.createTextNode(""));
-        document.head.appendChild(style);
-    }
+    // if(sheets.length == 0)
+    // {
+        // let style = document.createElement('style');
+        // style.appendChild(document.createTextNode(""));
+        // document.head.appendChild(style);
+    // }
 
-    let sheet = sheets[sheets.length - 1];
+    // let sheet = sheets[sheets.length - 1];
     // window.alert(CSSKeyframesRule.cssRules)
     // sheet.insertRule(ruleText, 1);
     // sheet.addRule("#myList li", "float: left; background: red !important;", 1);
@@ -99,7 +117,7 @@ function insertStyleSheetRule(ruleText){
     //     myReuseableStylesheet.insertRule(
     //         "@keyframes " + name + "{" + frames + "}", pos);
     // }
-}
+// }
 
 
 
@@ -133,5 +151,18 @@ function insertStyleSheetRule(ruleText){
 
 // stopAtSomeDeg(180);
 
-    
-
+// function insertStyleSheetRule(ruleText){
+//     // var style = document.createElement('style');
+//     // style.type = 'text/css';
+//     // var keyFrames = `@keyframes glow-2 { 0%, 90% { box-shadow: 0 0 20px 5px #c0392b; } 100% { box-shadow: none; } }`;
+//     // // style.innerHTML = keyFrames.replace(/A_DYNAMIC_VALUE/g, "180deg");
+//     // style.innerHTML = keyFrames
+//     // document.getElementsByTagName('head')[0].appendChild(style);
+//     var style = document.createElement('style');
+//     style.innerHTML = `
+//     #target {
+//     color: blueviolet;
+//     }
+//     `;
+//     document.head.appendChild(style);
+// }
